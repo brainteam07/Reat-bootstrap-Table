3a5392ae52f56643987c9d7518b0e9d126ed0f68
DESKTOP-KF57IBR\PRIYANKA <priyankasatwan@gmail.com>
2022-01-09 22:49:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import{ Table} from'react-bootstrap'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // const student =['priyanka','yogesh','anil','peter'];
+    // student.map((item)=>{
+    //     console.log("My Name is:",item)
+    // });
+    const student = [
 
-export default App;
+        { name: "Priyanka", email: "priyanka@gmail.com", id: 111 },
+        { name: "Daksh", email: "daksh@gmail.com", id: 222 },
+        { name: "anil", email: "anil@gmail.com", id: 333 },
+        { name: "nil", email: "nil@gmail.com", id: 111}
+    ]
+
+    return (
+        <div className='App'>
+            <h1>Handle array with list</h1>
+            <Table striped variant='dark' >
+            <tbody>
+                <tr>
+
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Id</td>
+                </tr>
+                {
+                    student.map((item,i) =>
+                
+                     <tr key={i}>
+                            <td>{item.name} </td>
+                            <td>{item.email} </td>
+                            <td>{item.id}</td>
+                        </tr>
+                       
+                 )
+            }
+            console.log(student);
+            </tbody>
+</Table>
+ </div>
+    )
+}
+export default App
